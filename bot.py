c2e8abff7237888533c47a7d04a8bf2ef53ba490
Shibashiba0909 <a990175972@gmail.com>
2022-06-15 23:25:54 +0800

    Capoo

--- bot.py
+++ bot.py
@@ -17,19 +17,6 @@ bot = commands.Bot(command_prefix='[',intents=intents)
 async def on_ready():
    print(">> bot is online <<")
 
-#機器人傳圖片
-@bot.command()
-async def 圖片(ctx):
-   random_pic = random.choice(jdata['pic'])
-   pic = discord.File(random_pic)
-   await ctx.send(random_pic)
-
-#機器人傳網路圖片
-@bot.command()
-async def web(ctx):
-   random_pic = random.choice(jdata['url_pic'])
-   await ctx.send(random_pic)
-
 #成員加入訊息
 @bot.event
 async def on_member_join(member):
@@ -42,12 +29,24 @@ async def on_member_remove(member):
    channel = bot.get_channel(int(jdata['Leave_channel']))
    await channel.send(f'{member} leave!')
 
+@bot.command()
+async def load(ctx, extension):
+   bot.load_extension(f'cmds.{extension}')
+   await ctx.send(f'Loaded {extension} done.')
+
+@bot.command()
+async def unload(ctx, extension):
+   bot.unload_extension(f'cmds.{extension}')
+   await ctx.send(f'Un - Loaded {extension} done.')
+
+@bot.command()
+async def reload(ctx, extension):
+   bot.reload_extension(f'cmds.{extension}')
+   await ctx.send(f'Re - Loaded {extension} done.')
 
 for filename in os.listdir('./cmds'):
    if filename.endswith('.py'):
       bot.load_extension(f'cmds.{filename[:-3]}')
 
-
 if __name__ == "__main__":
-
-bot.run(jdata['TOKEN'])
+   bot.run(jdata['TOKEN'])
--- cmds/rezct.py
+++ cmds/rezct.py
@@ -4,19 +4,19 @@ from core.classes import Cog_Extension
 import random
 import json
 
-with open('setting.json','r',encoding='utf8') as jfile:
-   jdata = json.load(jfile)
+with open('setting.json', 'r', encoding='utf8') as jfile:
+   jdata =json.load(jfile)
 
 class React(Cog_Extension):
 
     @commands.command()
-    async def 圖片(self,ctx):
+    async def 圖片(self, ctx):
        random_pic = random.choice(jdata['pic'])
        pic = discord.File(random_pic)
        await ctx.send(random_pic)
 
     @commands.command()
-    async def web(self,ctx):
+    async def web(self, ctx):
        random_pic = random.choice(jdata['url_pic'])
        await ctx.send(random_pic)
 
